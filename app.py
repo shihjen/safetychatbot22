78f141a6a7713a6c27c5ec25b373836a0ad26bd7
Tan Shih Jen <jen910604@gmail.com>
2024-05-04 21:17:43 +0800

    update code

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# import dependencies 
 import streamlit as st
 import os
 from dotenv import load_dotenv
@@ -11,16 +12,20 @@ from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_community.document_loaders import PyPDFDirectoryLoader
 
+# get the api key from .env file
 load_dotenv()
 os.environ['OPENAI_API_KEY']=os.getenv('OPENAI_API_KEY')
 groq_api_key=os.getenv('GROQ_API_KEY')
 huggingface_api_key = os.getenv('HUGGINGFACEHUB_API_TOKEN')
 
+# Streamlit UI
 st.title('Safety Chatbot22')
 st.write('Hello, I am Bot 22. I can assist you if you have any safety-related questions such as chemical, biological or radiation safety when working in the NUS laboratories. I am currently unable to access safety manuals on my own, but click the [Activate Me] button below and I will be ready to answer your questions.')
 
+# initialize the llm model --- llama3 
 llm=ChatGroq(groq_api_key=groq_api_key, model_name='Llama3-8b-8192')
 
+# create a prompt template
 prompt = ChatPromptTemplate.from_template(
 """
 Answer the questions based on the provided context only.
@@ -32,6 +37,7 @@ Questions: {input}
 """
 )
 
+# define a function which load the data, split data into chunks, perform text embeddings and store the embeddings in a vector database
 def vector_embedding():
     if 'vectors' not in st.session_state:
         st.session_state.embeddings=huggingFace_embeddings = HuggingFaceEmbeddings(model_name='BAAI/bge-small-en-v1.5', model_kwargs={'device':'cpu'}, encode_kwargs={'normalize_embeddings':False})
@@ -41,13 +47,16 @@ def vector_embedding():
         st.session_state.final_documents=st.session_state.text_splitter.split_documents(st.session_state.docs)
         st.session_state.vectors=FAISS.from_documents(st.session_state.final_documents, st.session_state.embeddings)
 
+# Streamlit UI for user to input their question
 prompt1 = st.chat_input('Please enter your question')
 
+# Streamlit UI for user to initialize the data loading, text splitting, embedding and create vector database
 if st.button('Activate Me'):
     st.write('Give me some time to read the safety manual. I will tell you when I have finished reading....')
     vector_embedding()
     st.write('I have finished reading the safety manuals. You may ask me your question now.')
 
+# data retrieval and answer user's query based on provided context
 if prompt1:
     st.write('User:', prompt1)
     document_chain=create_stuff_documents_chain(llm, prompt)
